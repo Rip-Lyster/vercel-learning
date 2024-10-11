4ce7e1591c349b6dd5f3bb9e70eba312bc80121f
Rip Lyster <riplyster@live.com>
2024-10-10 18:15:00 -0700

    fixed linting issues

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -2,30 +2,27 @@
 
 import Link from 'next/link';
 import { Montserrat } from 'next/font/google';
-import { useState } from 'react';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
 export default function Navigation() {
-  const [hovered, setHovered] = useState('');
-
-  return (
-    <nav className={`flex gap-6 items-center justify-center p-4 ${montserrat.className}`}>
-      <div className="align-bottom text-2xl font-[family-name:var(--font-symbols-regular)] mr-4">
-        gym bacteria
-      </div>
-      <Link 
-        href="/" 
-        className={`bacteria-link ${hovered === 'home' ? 'hovered' : ''}`}
-      >
-        Home
-      </Link>
-      <Link 
-        href="/about" 
-        className={`bacteria-link ${hovered === 'about' ? 'hovered' : ''}`}
-      >
-        About
-      </Link>
-    </nav>
-  );
-}
+    return (
+      <nav className={`flex gap-6 items-center justify-center p-4 ${montserrat.className}`}>
+        <div className="align-bottom text-2xl font-[family-name:var(--font-symbols-regular)] mr-4">
+          gym bacteria
+        </div>
+        <Link 
+          href="/" 
+          className="bacteria-link"
+        >
+          Home
+        </Link>
+        <Link 
+          href="/about" 
+          className="bacteria-link"
+        >
+          About
+        </Link>
+      </nav>
+    );
+  }
